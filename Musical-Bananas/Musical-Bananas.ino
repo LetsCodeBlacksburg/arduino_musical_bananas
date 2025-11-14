9b4fc08d766d97ad42143d7d4ac18c2ab93a8c81
tweeks-va <tweeks-homework@theweeks.org>
2025-11-14 01:25:16 -0500

    Worked out the issues.. not perfect..

--- Musical-Bananas/Musical-Bananas.ino
+++ Musical-Bananas/Musical-Bananas.ino
@@ -1,51 +1,60 @@
-// Msucial-Bananas
-// (c) 2025, tweeks-homework@theweeks.org
-// This program reads in the analog in pins 0-5, which should each
-// be hooked to a piece for fruit, such as a banana. We reach the 
-// input value of each Analog pin, and if a touch is detected, we
-// play that musical note.
+// Musical-Bananas
+// (c) 2025 tweeks-homework@theweeks.org
+// A fun little musical fruit organ (touch sensors) made from 3Mohm pull down
+// resistors into the Analog 0-5 pins, connection to pieces of fruit
+// to trigger the keys C-A (using pitches.h).  It's not a perfect quality 
+// sound with the constant polled reading of every analog input, but it's
+// good enough to get kids engaged. If anyone has a better key-polling method
+// please let me know.
 
 #include "pitches.h"
 
-int noteAPin=A0;
-int noteBPin=A1;
-int noteCPin=A2;
-int noteDPin=A3;
-int noteEPin=A4;
-int noteFPin=A5;
+//ANALOG MODDE
+int noteCPin=A0;
+int noteDPin=A1;
+int noteEPin=A2;
+int noteFPin=A3;
+int noteGPin=A4;
+int noteAPin=A5;
 
-int soundOutPin=5;
 
+int soundOutPin=8;
+int touchThresh=30;
 int note=0;
 
 void setup() {
   Serial.begin(9600);
-  
+  pinMode(soundOutPin, OUTPUT);
 }
 
 void loop() {
 
-  Serial.println("======================");
-  if(analogRead(noteAPin) > 600) note=NOTE_A3;
-  Serial.print("A=");Serial.println(analogRead(noteAPin));
-  if(analogRead(noteBPin) > 600) note=NOTE_B3;
-  Serial.print("B=");Serial.println(analogRead(noteBPin));
-  if(analogRead(noteCPin) > 600) note=NOTE_C4;
-  Serial.print("C=");Serial.println(analogRead(noteCPin));
-  if(analogRead(noteDPin) > 600) note=NOTE_D4;
-  Serial.print("D=");Serial.println(analogRead(noteDPin));
-  if(analogRead(noteEPin) > 600) note=NOTE_E4;
-  Serial.print("E=");Serial.println(analogRead(noteEPin));
-  if(analogRead(noteFPin) > 600) note=NOTE_F4;
-  Serial.print("F=");Serial.println(analogRead(noteFPin));
-  tone(soundOutPin, note);
-  delay(20);
+  // Use Serial prints for setting your touch-trigger threshold value.
+  //Serial.println("======================");
+  //delay(1);
+  if(analogRead(noteCPin) > touchThresh) note=NOTE_C4; 
+  //Serial.print("C=");Serial.println(analogRead(noteCPin));
+  //delay(1);
+  if(analogRead(noteDPin) > touchThresh) note=NOTE_D4; 
+  //Serial.print("D=");Serial.println(analogRead(noteDPin));
+  //delay(1);
+  if(analogRead(noteEPin) > touchThresh) note=NOTE_E4; 
+  //Serial.print("E=");Serial.println(analogRead(noteEPin));
+  //delay(1);
+  if(analogRead(noteFPin) > touchThresh) note=NOTE_F4; 
+  //Serial.print("F=");Serial.println(analogRead(noteFPin));
+  if(analogRead(noteGPin) > touchThresh) note=NOTE_G4; 
+  //Serial.print("G=");Serial.println(analogRead(noteGPin));
+  if(analogRead(noteAPin) > touchThresh) note=NOTE_A4; 
+  //Serial.print("A=");Serial.println(analogRead(noteFPin));
 
-//NOTE_A3
-//NOTE_B3
-//NOTE_C4
-//NOTE_D4
-//NOTE_E4
-//NOTE_F4
-  
+  if(note != 0){
+    tone(soundOutPin, note);
+    delay(40);
+    note=0;
+  }
+  else{
+    noTone(soundOutPin);    
+  }
+  delay(1);
 }
